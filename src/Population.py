a610963c7e74e3c4c1c69d1633f7c24d24d775b0
Zsombor Veres-Lakos <vl.zsombor@hotmail.com>
2025-02-04 08:58:30 +0100

    understanding code

--- src/Population.py
+++ src/Population.py
@@ -1,17 +1,28 @@
+import random
 import pygame
 
-from src.neat.AutoPlayer import AutoPlayer
-from .entities import (
-    Player,
-    Pipes
-)
-from .utils import Window
+from src.flappy import AutoPlayer
 
 
 class Population:
-    def __init__(self) -> None:
-        self.population: pygame.sprite.Group[Bird] = pygame.sprite.Group[Bird]()  # type: ignore # this has Birds
+    def __init__(self, population: pygame.sprite.Group[AutoPlayer], pop_size = 1) -> None: # type: ignore
+        self.population: pygame.sprite.Group[AutoPlayer] = pygame.sprite.Group[AutoPlayer]()  # type: ignore # this has Birds
         self.best_fitness = 0
+        self.pop_size = pop_size  # Size of population
+
+
+    def reset(self):
+        parents = self.population.sprites()
+
+        parents.sort(key = lambda x: x.fitness, reverse=True)
+        self.population.empty()
+
+        for i in range(self.pop_size):
+            parent1 = parents[random.randint(0,len(parents) // 10)]
+            parent2 = parents[random.randint(0, len(parents) // 10)]
+
+            bird = parent1.mate(parent2)
+
 
 
     # def update(self, pipes: Pipes, player: Player, window: Window):
--- src/neat/autoPlayer.py
+++ src/neat/autoPlayer.py
@@ -1,5 +1,6 @@
 import math
 from typing import Callable, List
+from typing import Self
 
 import pygame
 from src.entities import Player
@@ -11,14 +12,19 @@ from src.utils import GameConfig, Window
 
 
 class AutoPlayer(Player):
-    def __init__(self, config: GameConfig, gh: GeneHistory) -> None:
+    def __init__(self, config: GameConfig, gh: GeneHistory, clone: bool = False) -> None:
         super().__init__(config) # type: ignore
         self.gh = gh  # The genome history
         self.brain = Genome(gh)
+        self.fitness = 0
         # Random mutations for brain at start
-        for _ in range(10):
-            self.brain.mutate()
-    
+        if not clone:
+            for _ in range(10):
+                self.brain.mutate()
+    def mate(self, otherParent: Self):
+        child = AutoPlayer(self.config, self.gh)
+        child.brain = self.brain.crossover(partner.brain)
+        pass
 
     def update(self, pipes: Pipes, window: Window):
         # # if self.on_ground:
@@ -45,10 +51,9 @@ class AutoPlayer(Player):
     def think(self, pipes: Pipes, window: Window) -> bool:
         inputs = self.get_inputs(pipes, window)
         should_flap = False
-        sigmoid: Callable[[float], float] = lambda x: 1 / (1 + math.exp(-x))
         # Get outputs from brain
         outs = self.brain.get_outputs(inputs)
-        
+        #sigmoid: Callable[[float], float] = lambda x: 1 / (1 + math.exp(-x))
         # outs: List[float] =[0.89, sigmoid(inputs[0]*-0.922838921439954 + inputs[1] * 1.8011388502959025)]
         # with open("C:\\Users\\ZsomborVeres-Lakos\\Documents\\flappy_outputs.csv", 'a') as f:
         #     f.write(str(outs[1]) + '\n')
